615c32862071658107bc0b8b4114cc2d103757f3
Marlon Decosta <mad1083@gmail.com>
2019-05-04 21:54:45 -0400

    Delete diffArray.js

--- diffArray.js
+++
@@ -1,8 +0,0 @@
-function diffArray(arr1, arr2) {
-  if(arr1.length > arr2.length) {
-    return arr1.filter(val => !arr2.includes(val)) 
-  }
-  return arr2.filter(val => !arr1.includes(val))
-}
-
-console.log(diffArray([1,2,3,4,5,6], [1,2,3,4,5]))
